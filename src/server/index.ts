b38679f4f3d0546a5aff88fd88c177b826ccfac2
Hunter Parcells <hparcells@icloud.com>
2019-12-24 16:09:49 -0500

    Ending view count

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,10 +1,11 @@
 import { app } from 'fullstack-system';
+import r from 'rethinkdb';
 import fs from 'fs-extra';
 import path from 'path';
 
 import { initBot } from '../discord-bot/bot';
 import { getScene } from './scene';
-import { connectToDatabase } from './database';
+import { connectToDatabase, runDb, ctaDb } from './database';
 
 // Connect to the database
 connectToDatabase();
@@ -13,9 +14,20 @@ connectToDatabase();
 initBot();
 
 app.get('/api/scene/*', async (req, res) => {
+  const sceneName = req.url.substr(11);
   const scene = await getScene(req.url.substr(11));
 
   if (scene) {
+    if (scene.type === 'ending') {
+      // Increment view count.
+      runDb(
+        ctaDb()
+          .table('scenes')
+          .get(sceneName)
+          .update({ views: r.row('views').add(1) })
+      );
+    }
+
     res.send({ exists: true, scene });
   } else {
     res.send({ exists: false });
